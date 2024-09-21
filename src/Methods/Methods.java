f6ffe8c5f2d8ac1a5db776ea242a9774d51b7e19
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-21 18:58:19 +0530

    defining methods in a java

--- src/Methods/Methods.java
+++ src/Methods/Methods.java
@@ -7,6 +7,11 @@ public class Methods {
 
         int[] arr2 = {7,8,5,9,7,3,4,1,2,0,3,6,5,8,9,6,4,7};
         System.out.println(sumOfArray(arr2));
+
+        System.out.println(trimAndUpper("         Electrical      "));
+
+        System.out.println(sum(1, 5));
+        System.out.println(sum(190, 35, 1));
     }
 
     public static int sumOfArray(int[] arr) {
@@ -17,5 +22,20 @@ public class Methods {
         }
 
         return sum;
+    }
+
+    private static String trimAndUpper(String str) {
+        return str.trim().toUpperCase();
+    }
+
+    // METHOD OVERLOADING
+    // Here, two methods with exact same definitions have been defined but only the number of parameters have changed.
+    // This is called method overloading.
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+
+    private static int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
