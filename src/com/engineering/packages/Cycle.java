1780dd504b3789eaac2ecf1741d4727a4831f261
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-29 21:10:59 +0530

    Packages in Java

---
+++ src/com/engineering/packages/Cycle.java
@@ -0,0 +1,11 @@
+package com.engineering.packages;
+
+public class Cycle {
+    // the name of the public class should be same as that of the file.
+}
+
+// there can be multiple files in a file.
+class MiniCycle {
+    // however, this class can only be accessed within this package and not the outer
+    // packages.
+}
