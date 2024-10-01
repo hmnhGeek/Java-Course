6319f8f3bd7a63b5f803e8aabff008b5b6d019d4
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-01 20:35:13 +0530

    super() keyword

--- src/humans/Parent.java
+++ src/humans/Parent.java
@@ -2,6 +2,9 @@ package humans;
 
 public class Parent extends GrandParent {
     public Parent() {
+        super(); // this function calls the constructor of the parent class and is hidden always.
+        // this function should always be the first to be called in the constructor of the child
+        // class. Note that it is useful when there is no default constructor in the parent class.
         System.out.println("Parent() constructor called.");
     }
 }
