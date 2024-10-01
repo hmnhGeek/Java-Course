faedf57c3b132d2b87b48af51e2daa9913e3af49
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-01 20:45:43 +0530

    @Override annotation

--- src/animals/Dog.java
+++ src/animals/Dog.java
@@ -1,6 +1,9 @@
 package animals;
 
 public class Dog extends Animal {
+    // @Override annotation helps in avoiding error and ensures that certain methods of parent class
+    // are implemented in the child class.
+    @Override
     public void sayHello() {
         System.out.println("Woof woof!");
     }
--- src/humans/Test.java
+++ src/humans/Test.java
@@ -3,6 +3,9 @@ package humans;
 public class Test {
     public static void main(String[] args) {
         // Multi-level Inheritance; Child <--- Parent <--- GrandParent
+        //
+        // Note that `new` keyword is used to dynamically create object during runtime and not on
+        // compile time.
         Child child = new Child(); // this will also print the order of constructor calls.
         // please note that only the properties are inherited and not the actual values.
         child.setName("Ram"); // this is the name of the `child` and not that of a GrandParent.
