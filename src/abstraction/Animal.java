fde2b4a24a3920a3f3a1d1643dbede7cd0559b8d
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-03 20:10:31 +0530

    Abstraction in Java

--- src/abstraction/Animal.java
+++ src/abstraction/Animal.java
@@ -2,8 +2,10 @@ package abstraction;
 
 public abstract class Animal {
     // an abstract method does require an abstract class. This helps us to avoid writing
-    // unnecessary code in parent class.
-    public abstract void sayHello();
+    // unnecessary code in parent class. Also note that `protected` access modifier is
+    // used to specify that this method can only be accessed by those classes which extend
+    // this class. `protected` keyword can be used on abstract as well as on a normal class.
+    protected abstract void sayHello();
 
     // an abstract class can have normal declared and defined methods as well.
     // Basically these methods are called concrete methods.
